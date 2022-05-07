b381eda8d80ce01f454ed1d07e46086b18b3a8bf
Ola <kozlowska.aleksandra92@gmail.com>
2022-05-07 19:42:07 +0200

    Rozpoczęcie zadania

---
+++ src/test/java/pl/akoz/zadanie25/Zadanie25ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akoz.zadanie25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
